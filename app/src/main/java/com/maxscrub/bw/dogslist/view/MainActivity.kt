e5136ab87b762f7430f91495673935efc9d18580
BW <bmwanz@live.com>
2020-07-20 17:12:52 -0700

    start navigation

--- app/src/main/java/com/maxscrub/bw/dogslist/view/MainActivity.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.maxscrub.bw.dogslist
+package com.maxscrub.bw.dogslist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maxscrub.bw.dogslist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
