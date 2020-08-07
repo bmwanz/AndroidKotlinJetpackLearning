2306e70a9d9053281861e3cef958eaee09997b04
BW <bmwanz@live.com>
2020-08-06 18:03:15 -0700

    layouts + clean up

--- app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.maxscrub.bw.dogslist.R
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -28,15 +26,6 @@ class DetailFragment : Fragment() {
         arguments?.let {
             // retrieve argument passed from ListFragment
             dogUUID = DetailFragmentArgs.fromBundle(it).dogUUID
-            detailFragText.text = dogUUID.toString()
-        }
-
-        buttonList.setOnClickListener{
-            val action = DetailFragmentDirections.actionToListFrag()
-            Navigation.findNavController(it).navigate(action)
-
-            // navigation will handle swapping fragment
-            // if res/navigation/dog_navigation is set up correctly
         }
     }
 }
--- app/src/main/java/com/maxscrub/bw/dogslist/view/ListFragment.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/ListFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.maxscrub.bw.dogslist.R
-import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -17,20 +15,5 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        buttonDetails.setOnClickListener{
-            val action = ListFragmentDirections.actionToDetailFrag()
-            action.dogUUID = 5
-            Navigation.findNavController(it).navigate(action)
-
-            // navigation will handle swapping fragment
-            // if res/navigation/dog_navigation is set up correctly
-
-            // passing argument dogUUID = 5 to DetailFragment
-        }
     }
 }
