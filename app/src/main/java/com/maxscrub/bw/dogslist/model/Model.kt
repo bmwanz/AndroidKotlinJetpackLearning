6d5d3a07710a87260042a4ac97b61f4d742b74c4
BW <bmwanz@live.com>
2020-08-11 17:59:57 -0700

    start list adapter and view model

---
+++ app/src/main/java/com/maxscrub/bw/dogslist/model/Model.kt
@@ -0,0 +1,11 @@
+package com.maxscrub.bw.dogslist.model
+
+data class DogBreed(
+    val breedID: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
--- app/src/main/java/com/maxscrub/bw/dogslist/view/ListFragment.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/ListFragment.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.maxscrub.bw.dogslist.R
+import com.maxscrub.bw.dogslist.model.DogBreed
+import com.maxscrub.bw.dogslist.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel : ListViewModel
+    private val dogsListAdapter = DogsListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +25,50 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // instantiate view model
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        // instantiate dogs list
+        dogsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = dogsListAdapter
+        }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        // observe mutable live data variables
+        viewModel.dogs.observe(this, Observer {dogs ->
+            dogs?.let {
+                dogsList.visibility = View.VISIBLE
+                dogsListAdapter.updateDogList(dogs)
+            }
+        })
+
+        viewModel.dogsLoadError.observe(this, Observer {isError ->
+            isError?.let {
+                listError.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                // if is loading, show spinner, else hide
+                loadingView.visibility = if(it) View.VISIBLE else View.GONE
+
+                if (it) {
+                    // if is loading, hide everything else
+                    listError.visibility = View.GONE
+                    dogsList.visibility = View.GONE
+                }
+            }
+        })
     }
 }
