f692af0ba7c9a0620b613bb8cd99c314b649eaed
BW <bmwanz@live.com>
2020-08-31 23:23:21 -0700

    databinding for detail frag

--- app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.maxscrub.bw.dogslist.R
+import com.maxscrub.bw.dogslist.databinding.FragmentDetailBinding
+import com.maxscrub.bw.dogslist.databinding.FragmentDetailBindingImpl
 import com.maxscrub.bw.dogslist.util.getProgressDrawable
 import com.maxscrub.bw.dogslist.util.loadImage
 import com.maxscrub.bw.dogslist.viewmodel.DetailViewModel
@@ -17,13 +20,15 @@ class DetailFragment : Fragment() {
 
     private lateinit var viewModel: DetailViewModel
     private var dogUUID = 0;
+    private lateinit var dataBindingL: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBindingL = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBindingL.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,11 +49,7 @@ class DetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let {
-                dogName.text = dog.dogBreed
-                dogPurpose.text = dog.bredFor
-                dogTemperament.text = dog.temperament
-                dogLifeSpan.text = dog.lifeSpan
-                context?.let {dogImage.loadImage(dog.imageUrl, getProgressDrawable(it)) }
+                dataBindingL.dog = dog
             }
         })
     }
