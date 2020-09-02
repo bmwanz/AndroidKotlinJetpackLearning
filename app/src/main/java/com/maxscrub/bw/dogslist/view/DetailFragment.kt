0aa48a65448d00a4709d98cb44b4c8823d31c70c
BW <bmwanz@live.com>
2020-09-01 22:21:57 -0700

    palatte

--- app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.maxscrub.bw.dogslist.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +10,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.maxscrub.bw.dogslist.R
 import com.maxscrub.bw.dogslist.databinding.FragmentDetailBinding
 import com.maxscrub.bw.dogslist.databinding.FragmentDetailBindingImpl
+import com.maxscrub.bw.dogslist.model.DogPalette
 import com.maxscrub.bw.dogslist.util.getProgressDrawable
 import com.maxscrub.bw.dogslist.util.loadImage
 import com.maxscrub.bw.dogslist.viewmodel.DetailViewModel
@@ -50,7 +57,29 @@ class DetailFragment : Fragment() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let {
                 dataBindingL.dog = dog
+
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap().load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            // if rgb is null, color is 0 (default)
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            val myPalette = DogPalette(intColor)
+                            dataBindingL.palatte = myPalette
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+            })
     }
 }
