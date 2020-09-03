3f36d9f8a496f41907e204aefe2b589095600f50
BW <bmwanz@live.com>
2020-09-02 22:51:50 -0700

    begin preferences

---
+++ app/src/main/java/com/maxscrub/bw/dogslist/view/SettingsFragment.kt
@@ -0,0 +1,16 @@
+package com.maxscrub.bw.dogslist.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+import com.maxscrub.bw.dogslist.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
